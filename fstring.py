368414a3925aedff4a2591fe1c5370efcd3e23ef
singlstrand <singlstrand@protonmail.com>
2019-11-02 16:46:00 +0530

    first commit

---
+++ fstring.py
@@ -0,0 +1,6 @@
+father = 'Kiran'
+mother = 'Aruna'
+kid = 'Anika'
+
+x=f'The father and mother of the kid {kid} are {father} and {mother} respectively'
+print('this is going no where {x}'.format(x))

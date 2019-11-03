05fdfd89abc54b59d8f1bb13634c39cc5d06db4a
singlstrand <singlstrand@protonmail.com>
2019-11-04 01:25:17 +0530

    second commit

--- fstring.py
+++ fstring.py
@@ -3,4 +3,4 @@ mother = 'Aruna'
 kid = 'Anika'
 
 x=f'The father and mother of the kid {kid} are {father} and {mother} respectively'
-print('this is going no where {x}'.format(x))
+print('this is going no where {}'.format(x))
